8a356c8b4d6169269f56f7be6630b47f7ee7f436
BubbaGodinet <godinetvn@gmail.com>
2021-12-02 11:46:44 -0700

    Finished all deliverables except the final 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,20 @@
+puts 'Clearing old data'
+
+Company.destroy_all
+Dev.destroy_all
+Freebie.destroy_all
+
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+c1 = Company.create(name: "Google", founding_year: 1998)
+c2 = Company.create(name: "Facebook", founding_year: 2004)
+c3 = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+c4 = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+d1 = Dev.create(name: "Rick")
+d2 = Dev.create(name: "Morty")
+d3 = Dev.create(name: "Mr. Meseeks")
+d4 = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -16,6 +22,12 @@ puts "Creating freebies..."
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
-# Create freebies Here
+
+f1 = Freebie.create(item_name: 'Hacky Sack', value: 3 , dev_id: d1.id, company_id:c1.id )
+f2 = Freebie.create(item_name: 'Beanie', value: 8, dev_id: d2.id, company_id: c2.id )
+f3 = Freebie.create(item_name: 'Hat', value: 10, dev_id: d3.id, company_id: c3.id )
+f4 = Freebie.create(item_name: 'Longboard', value: 150, dev_id: d4.id, company_id: c4.id )
+f5 = Freebie.create(item_name: 'Trip for Two', value: 1000, dev_id: d4.id, company_id: c4.id )
+
 
 puts "Seeding done!"
