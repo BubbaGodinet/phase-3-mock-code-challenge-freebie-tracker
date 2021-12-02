9af52bcd0b88430aa262665f7407052cc13fd35a
BubbaGodinet <godinetvn@gmail.com>
2021-12-02 12:09:55 -0700

    finished received_one? deliverable

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,7 +3,7 @@ class Company < ActiveRecord::Base
     has_many :devs, through: :freebies
 
     def give_freebie(dev, item_name, value)
-        Freebie.create(dev: dev, item_name: item_name, value: value)
+        Freebie.create(company: self.id, dev: dev, item_name: item_name, value: value)
     end
 
     def self.oldest_company
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -3,10 +3,11 @@ class Dev < ActiveRecord::Base
     has_many :companies, through: :freebies
 
     def received_one?(item_name)
-        
+      matches = self.freebies.filter{|freebie| freebie.item_name == item_name}
+      matches.length > 0 ? true : false
     end
 
     def give_away(dev, freebie)
-
+        
     end
 end
